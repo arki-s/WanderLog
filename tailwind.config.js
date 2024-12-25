ae6719f582ba7d297f75e7501cc83b3a3513fb41
exec zsh <wind10cloud@gmail.com>
2024-12-25 18:04:06 +0900

    added a primary button and star ratings to the create page

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,31 @@ export default {
       colors: theme.colors,
     },
   },
-  plugins: [],
-}
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        '.btn': {
+          'padding': '0.5rem 1rem',
+          'border-radius': '50px',
+          'font-weight': '600',
+          'transition': 'all 0.3s',
+          'min-width':'10.5rem',
+          'text-align' : 'center',
+          'font-size' : '14px',
+          'cursor': 'pointer',
+        },
+        '.btn-primary': {
+          'background-color': '#78C1F3',
+          'color': '#F8FDCF',
+          '&:hover': {
+            'background-color': '#9BE8D8',
+          },
+          '&:focus': {
+            'outline': '2px solid #bfdbfe',
+          },
+        },
+      });
+    },
 
+  ],
+}
