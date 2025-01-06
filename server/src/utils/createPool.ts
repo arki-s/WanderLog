0ba9e127fb497b52aedfbafc6f2da27d0e1e0217
exec zsh <wind10cloud@gmail.com>
2025-01-06 17:37:03 +0900

    Installed mysql2 and added a createPool to the server

---
+++ server/src/utils/createPool.ts
@@ -0,0 +1,12 @@
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const pool = mysql.createPool({
+    connectionLimit: 100,
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+});
