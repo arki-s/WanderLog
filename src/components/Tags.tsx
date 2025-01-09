64e331eca4fbace2415994a5ce25e9bf0f8e5e7c
exec zsh <wind10cloud@gmail.com>
2025-01-09 16:28:42 +0900

    Added mock data to the Tags page

--- src/components/Tags.tsx
+++ src/components/Tags.tsx
@@ -3,6 +3,22 @@ import '../App.css';
 
 export const Tags = () => {
   const [modal, setModal] = useState(false);
+  const mockTags = [
+    { id: 1, name: "サンプルタグ" },
+    { id: 2, name: "サンプルタグ2" },
+    { id: 3, name: "サンプルタグ3" },
+    { id: 4, name: "サンプルタグ4" }
+  ];
+
+  const tagList = mockTags.map((tag) => {
+    return (
+      <div key={tag.id} className='flex justify-start gap-2 mb-2'>
+        <p>{tag.name}</p>
+        <button className='btn btn-primary'>編集</button>
+        <button className='btn btn-cancel'>削除</button>
+      </div>
+    )
+  });
 
   const tagModal = (
     <div className="modalContainer">
@@ -23,6 +39,9 @@ export const Tags = () => {
       <div className="p-1 md:p-3">
         <h1 className='pb-5'>Tags</h1>
         <button className='btn btn-primary' onClick={() => setModal(true)}>新しいタグを作成</button>
+        <div className='py-2'>
+          {tagList}
+        </div>
         {modal && tagModal}
 
       </div>
