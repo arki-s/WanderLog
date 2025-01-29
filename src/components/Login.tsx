0ec62f711397f681fc0a07c3a1bca42732b5fc23
exec zsh <wind10cloud@gmail.com>
2025-01-29 21:23:38 +0900

    added usestate to the login page

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,14 +1,24 @@
+import { useState } from "react"
+
 const Login = () => {
+  const [user, setUser] = useState({
+    email: "",
+    password: ""
+  });
+
+  console.log(user);
+
+
   return (
     <div className='relative w-screen pt-20 md:pt-16'>
       <div className='my-5 mx-auto' style={{ width: '80%' }}>
         <h1 className='font-bold mb-5'>Login</h1>
         <div className="pb-3">
-          <input type="text" placeholder='メールアドレスを入力' className="border border-gray-400 px-3 py-2 rounded-lg" />
+          <input type="text" onChange={(e) => { setUser({ ...user, email: e.target.value }) }} placeholder='メールアドレスを入力' className="border border-gray-400 px-3 py-2 rounded-lg" />
         </div>
 
         <div className="pb-4">
-          <input type="password" placeholder='パスワードを入力' className="border border-gray-400 px-3 py-2 rounded-lg" />
+          <input type="password" onChange={(e) => { setUser({ ...user, email: e.target.value }) }} placeholder='パスワードを入力' className="border border-gray-400 px-3 py-2 rounded-lg" />
         </div>
 
         <div className="w-1/4 p-2">
