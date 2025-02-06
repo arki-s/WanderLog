f99373b14f740e687e8dc222967f7fb7de625783
exec zsh <wind10cloud@gmail.com>
2025-02-06 20:06:48 +0900

    added auth router to the server

--- server/src/routes/mainRouter.ts
+++ server/src/routes/mainRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import tagsRouter from "./tagsRouter";
+import authRouter from "./authRouter";
 
 const mainRouter = Router();
 
 mainRouter.use('/tags', tagsRouter);
+mainRouter.use('/auth', authRouter);
 
 export default mainRouter;
